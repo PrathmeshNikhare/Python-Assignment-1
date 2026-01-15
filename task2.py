6e59d74643f85f97ac3275bf9e48c11b02487e40
PrathmeshNikhare <prathamesh.nikhare17@gmail.com>
2026-01-15 12:51:23 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,5 @@
+fname = str(input("Enter your first name: "))
+lname = str(input("Enter your last name: "))
+fullname = fname + " " + lname
+
+print("Hello, ", fullname, "! Welcome to the Python program", sep = "")
